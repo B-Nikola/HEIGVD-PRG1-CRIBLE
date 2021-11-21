1fdfa530da21a5d13796d802a19ac328bea53d61
B-Nikola <nikola.bouattit@gmail.com>
2021-11-21 16:09:53 +0100

    [affichageTableau]Correction of display

--- affichageTableau.cpp
+++ affichageTableau.cpp
@@ -7,9 +7,7 @@ Date creation  : 19.11.2021
 
 Description    : Les fonctions permettant l'affichage de la matrice basé sur un
                  tableau 1 dimension
-
 Remarque(s)    : Seulement les tableaux a une seule dimension sont supporté
-
 
 Assertions     : n/a
 
@@ -27,19 +25,19 @@ Compilateur    : Mingw-w64 g++ 11.2.0
 const char REPRESENTANT = '0'; // Charactere pour l'affichage de la matrice
 
 
-void afficherPremier( unsigned taille, const int tab[], unsigned nbColonne,
+void afficherTableau( unsigned taille, const int tab[], unsigned nbColonne,
                      char charSpecial  ){
    int caseTableau = 0;
    const int ESPACE_AFFICHAGE = 3;
 
    for ( int  ligne = 1; ligne <= taille; ) {
       for (int colonne = 0; colonne < nbColonne; ++colonne) {
-         if (ligne==tab[caseTableau])
+         if (ligne==tab[caseTableau] )
          {
-            std::cout << std::fixed <<std::left << std::setw(ESPACE_AFFICHAGE) << charSpecial ;
+            std::cout << std::fixed <<std::left << std::setw(ESPACE_AFFICHAGE) << REPRESENTANT ;
             ++caseTableau;
          }else{
-            std::cout << std::fixed <<std::left << std::setw(ESPACE_AFFICHAGE) << REPRESENTANT;
+            std::cout << std::fixed <<std::left << std::setw(ESPACE_AFFICHAGE) << charSpecial;
          }
          ++ligne;
       }
@@ -49,7 +47,6 @@ void afficherPremier( unsigned taille, const int tab[], unsigned nbColonne,
 
 
 void afficherTableau( unsigned taille, unsigned nbColonne){
-   int caseTableau = 0;
    const int ESPACE_AFFICHAGE = 3;
    for ( int  ligne = 0; ligne < taille; ++ligne) {
       for (int colonne = 0; colonne < nbColonne; ++colonne) {
