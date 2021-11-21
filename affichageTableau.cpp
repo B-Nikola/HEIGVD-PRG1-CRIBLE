25c853f8ec56ef5ad655733b7a39cd65fb4e6ce4
B-Nikola <nikola.bouattit@gmail.com>
2021-11-21 15:35:00 +0100

    [affichageTableau] added library

---
+++ affichageTableau.cpp
@@ -0,0 +1,68 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier : affichage.cpp
+Nom du labo    : HEIGVD-PRG1-CRIBLE
+Auteur(s)      : Jérémie Métrailler, Nikola Bouattit
+Date creation  : 19.11.2021
+
+Description    : Les fonctions permettant l'affichage de la matrice basé sur un
+                 tableau 1 dimension
+
+Remarque(s)    : Seulement les tableaux a une seule dimension sont supporté
+
+
+Assertions     : n/a
+
+Modifications  : n/a
+
+Compilateur    : Mingw-w64 g++ 11.2.0
+-----------------------------------------------------------------------------------
+*/
+
+#include "affichageTableau.h"
+#include <iostream>     //Nécessaire pour l'écriture dans la console
+#include <iomanip>      //Nécessaire pour limiter l'affichage
+
+
+const char REPRESENTANT = '0'; // Charactere pour l'affichage de la matrice
+
+
+void afficherPremier( unsigned taille, const int tab[], unsigned nbColonne,
+                     char charSpecial  ){
+   int caseTableau = 0;
+   const int ESPACE_AFFICHAGE = 3;
+
+   for ( int  ligne = 1; ligne <= taille; ) {
+      for (int colonne = 0; colonne < nbColonne; ++colonne) {
+         if (ligne==tab[caseTableau])
+         {
+            std::cout << std::fixed <<std::left << std::setw(ESPACE_AFFICHAGE) << charSpecial ;
+            ++caseTableau;
+         }else{
+            std::cout << std::fixed <<std::left << std::setw(ESPACE_AFFICHAGE) << REPRESENTANT;
+         }
+         ++ligne;
+      }
+      std::cout << std::endl;
+   }
+}
+
+
+void afficherTableau( unsigned taille, unsigned nbColonne){
+   int caseTableau = 0;
+   const int ESPACE_AFFICHAGE = 3;
+   for ( int  ligne = 0; ligne < taille; ++ligne) {
+      for (int colonne = 0; colonne < nbColonne; ++colonne) {
+         std::cout << std::fixed <<std::left << std::setw(ESPACE_AFFICHAGE) << REPRESENTANT;
+         ++ligne;
+      }
+      std::cout << std::endl;
+   }
+}
+
+void listerTableau(unsigned taille, const int tab[]){
+   std::cout << "il y a "<< taille << " nombres premiers qui sont : ";
+   for (unsigned int i = 0; i < taille; ++i) {
+      std::cout << tab[i] << " ";
+   }
+}
---
+++ affichageTableau.h
@@ -0,0 +1,48 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier : affichageTableau.h
+Nom du labo    : HEIGVD-PRG1-CRIBLE
+Auteur(s)      : Jérémie Métrailler, Nikola Bouattit
+Date creation  : 19.11.2021
+
+Description    : Librairie permettant d'afficher les contenue d'un tableau
+
+Remarque(s)    : Cette librairie a été spécifiquement péparer pour la lecture de
+                 tableau contenant des valeurs numériques comme des chiffre premiers
+
+Assertions     : n/a
+
+Modifications  : n/a
+
+Compilateur    : Mingw-w64 g++ 11.2.0
+-----------------------------------------------------------------------------------
+*/
+
+#ifndef HEIGVD_PRG1_CRIBLE_AFFICHAGETABLEAU_H
+#define HEIGVD_PRG1_CRIBLE_AFFICHAGETABLEAU_H
+/**
+ * Affiche une matrice ou pour chaque valeur du tableau fournis le signale par un
+ * charactere spécial
+ * @param taille Taille du tableau
+ * @param tab Tableau a fournir
+ * @param nbColonne Nombre de colonne pour l'affichage
+ * @param charSpecial Charactere spéciale pour signaler une valeur égale au tableau
+ */
+void afficherTableau( unsigned taille, const int tab[] ,unsigned nbColonne = 10,
+                      char charSpecial = 'X');
+/**
+ * Affiche une matrice d'une certaine taille et colonne
+ * @param taille Taille de la matrice a générée
+ * @param nbColonne nombre de colonne (par défaut 10)
+ */
+void afficherTableau(unsigned taille, unsigned nbColonne=10);
+/**
+ * Affiche tous les élément contenue dans un tableau
+ * @param taille Nombre d'élément actuellement dans le tableau
+ * @param tab Tableau de donnée a lire
+ */
+void listerTableau(unsigned taille, const int tab[]);
+
+
+
+#endif //HEIGVD_PRG1_CRIBLE_AFFICHAGETABLEAU_H
