3e62b33daf398ce0fb0a7dfe8390e9904975909c
B-Nikola <nikola.bouattit@gmail.com>
2021-11-22 18:11:30 +0100

    Correction warning

--- affichageTableau.cpp
+++ affichageTableau.cpp
@@ -1,6 +1,6 @@
 /*
 -----------------------------------------------------------------------------------
-Nom du fichier : affichage.cpp
+Nom du fichier : affichageTableau.cpp
 Nom du labo    : HEIGVD-PRG1-CRIBLE
 Auteur(s)      : Jérémie Métrailler, Nikola Bouattit
 Date creation  : 19.11.2021
@@ -9,6 +9,8 @@ Description    : Les fonctions permettant l'affichage de la matrice basé sur un
                  tableau 1 dimension
 
 Remarque(s)    : Seulement les tableaux a une seule dimension sont supporté
+                 Les matriche affiche pour chaque ligne le nombre maximal de
+                 colonne
 
 Assertions     : n/a
 
@@ -23,40 +25,44 @@ Compilateur    : Mingw-w64 g++ 11.2.0
 #include <iomanip>      //Nécessaire pour limiter l'affichage
 
 
-const char REPRESENTANT = '0'; // Charactere pour l'affichage de la matrice
-const int ESPACE_AFFICHAGE = 3;
+const char REPRESENTANT = '0';   // Charactere pour l'affichage de la matrice
+const int ESPACE_AFFICHAGE = 3;  // Espace définie pour un affichage constant
 
-void afficherCase( const int ESPACE_AFFICHAGE,char charCase);
+using namespace std;
+
 
 void afficherTableau(const int tab[], unsigned taille, unsigned nbColonne,
                      char charSpecial){
-   int caseTableau = 0;
+   unsigned caseTableau = 0;
    if(nbColonne == 0)
    {
-      for (int ligne = 1; ligne <= taille; ++ligne) {
-         if (ligne == tab[caseTableau]) {
-            afficherCase(ESPACE_AFFICHAGE, REPRESENTANT);
+      for (unsigned ligne = 1; ligne <= taille; ++ligne) {
+         if (ligne == (unsigned)tab[caseTableau]) {
+            cout   << fixed <<left
+                        << setw(ESPACE_AFFICHAGE) << REPRESENTANT;
             ++caseTableau;
          } else {
-
-            afficherCase(ESPACE_AFFICHAGE, charSpecial);
+            cout   << fixed <<left
+                        << setw(ESPACE_AFFICHAGE) << charSpecial;
          }
       }
-      std::cout << std::endl;
+      cout << endl;
    }
    else {
-      for (int ligne = 1; ligne <= taille;) {
-         for (int colonne = 0; colonne < nbColonne; ++colonne) {
-            if (ligne == tab[caseTableau]) {
-               afficherCase(ESPACE_AFFICHAGE, REPRESENTANT);
+      for (unsigned ligne = 1; ligne <= taille;) {
+         for (unsigned colonne = 0; colonne < nbColonne; ++colonne) {
+            if (ligne == (unsigned)tab[caseTableau]) {
+               cout   << fixed << left
+                           << setw(ESPACE_AFFICHAGE) << REPRESENTANT;
                ++caseTableau;
             } else {
-               afficherCase(ESPACE_AFFICHAGE, charSpecial);
+               cout     << fixed << left
+                        << setw(ESPACE_AFFICHAGE) << charSpecial;
             }
             ++ligne;
 
          }
-         std::cout << std::endl;
+         cout << endl;
       }
    }
 }
@@ -64,28 +70,25 @@ void afficherTableau(const int tab[], unsigned taille, unsigned nbColonne,
 
 void afficherTableau( unsigned taille, unsigned nbColonne){
    if(nbColonne == 0){
-      for(int i =0;i<taille;++i){
-         afficherCase(ESPACE_AFFICHAGE, REPRESENTANT);
+      for(unsigned i =0; i < taille;++i){
+         cout   << fixed << left
+                     << setw(ESPACE_AFFICHAGE) << REPRESENTANT;
       }
    }
    else {
-      for (int ligne = 0; ligne < taille; ++ligne) {
-         for (int colonne = 0; colonne < nbColonne; ++colonne) {
-            afficherCase(ESPACE_AFFICHAGE, REPRESENTANT);
+      for (unsigned ligne = 0; ligne < taille; ++ligne) {
+         for (unsigned colonne = 0; colonne < nbColonne; ++colonne) {
+            cout   << fixed << left
+                        << setw(ESPACE_AFFICHAGE) << REPRESENTANT;
             ++ligne;
          }
-         std::cout << std::endl;
+         cout << endl;
       }
    }
 }
 
 void ecrireTableau(unsigned taille, const int tab[]) {
    for (unsigned int i = 0; i < taille; ++i) {
-      std::cout << std::fixed <<std::left << std::setw(ESPACE_AFFICHAGE) << tab[i] << " ";
+      cout << fixed << left << setw(ESPACE_AFFICHAGE) << tab[i];
    }
-}
-
-void afficherCase( const int ESPACE_AFFICHAGE,char charCase){
-
-   std::cout << std::fixed <<std::left << std::setw(ESPACE_AFFICHAGE) << charCase;
 }
